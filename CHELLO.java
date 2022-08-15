b384588a162bfcdaf4c9450c53c9604904271b65
Jimmy Nguyễn <84088181+cityuongbi@users.noreply.github.com>
2022-08-15 20:30:06 +0700

    Update

---
+++ CHELLO.java
@@ -0,0 +1,11 @@
+// Created by Nguyễn Mạnh Quân
+
+import java.util.*;
+
+public class CHELLO
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello PTIT.");
+    }
+}
