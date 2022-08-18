9530316b190fbf1ef9a80b9533783e1e8e7aeb8b
Jimmy Nguyễn <84088181+cityuongbi@users.noreply.github.com>
2022-08-19 00:00:14 +0700

    Src code

---
+++ J01011 - BỘI SỐ CHUNG - ƯỚC SỐ CHUNG.java
@@ -0,0 +1,26 @@
+// Created by Nguyễn Mạnh Quân
+
+import java.util.*;
+
+public class J01011
+{
+
+    static long gcd(long a, long b)
+    {
+        if(b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            long a = sc.nextLong(), b = sc.nextLong();
+            long GCD = gcd(a, b);
+            System.out.println(a * b / GCD + " " + GCD);
+        }
+    }
+}
