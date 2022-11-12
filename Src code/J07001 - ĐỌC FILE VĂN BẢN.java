59983f0b2c4ed70159ce28840009b1f414ae2b93
Jimmy Nguyễn <quannm.b20cn550@stu.ptit.edu.vn>
2022-11-12 17:57:00 +0700

    Update

---
+++ Src code/J07001 - ĐỌC FILE VĂN BẢN.java
@@ -0,0 +1,14 @@
+// Created by Nguyễn Mạnh Quân at 17:48 on 12/11/2022
+
+import java.io.*;
+import java.util.*;
+
+public class J07001
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner sc = new Scanner(new File("DATA.in"));
+        while (sc.hasNextLine())
+            System.out.println(sc.nextLine());
+    }
+}
