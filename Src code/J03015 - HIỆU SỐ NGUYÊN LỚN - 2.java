48f9178dabac225ebf988480f2639822fb77268a
Jimmy Nguyễn <quannm.b20cn550@stu.ptit.edu.vn>
2022-11-15 01:55:20 +0700

    Update

---
+++ Src code/J03015 - HIỆU SỐ NGUYÊN LỚN - 2.java
@@ -0,0 +1,15 @@
+// Created by Nguyễn Mạnh Quân at 01:53 on 15/11/2022
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class J03015
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+        System.out.println(a.subtract(b));
+    }
+}
