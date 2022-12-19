898b01a8bc243d02fa9f9b32deb5c4dc47a003cf
Jimmy Nguyễn <quannm.b20cn550@stu.ptit.edu.vn>
2022-12-19 22:50:30 +0700

    Update

---
+++ Src code/J07078 - TÌM VỊ TRÍ XÂU CON.java
@@ -0,0 +1,26 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        Scanner sc = new Scanner(new File("STRING.in"));
+        int n = Integer.parseInt(sc.nextLine());
+        while (n-- > 0)
+        {
+            String a = sc.nextLine();
+            String b = sc.nextLine();
+            if (a.length() >= b.length())
+            {
+                int len = a.length() - b.length();
+                for (int i = 0; i <= len; ++i)
+                    if (b.equals(a.substring(i, i + b.length())))
+                        System.out.print((i + 1) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
